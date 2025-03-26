4c6887dd43882c21bd2876a520cbb3439c9565aa
“Temuujin” <temuujinbatdorj@gmail.com>
2025-03-26 11:59:29 +0800

    3

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
 
-// Function to connect to the MongoDB database
 const connectDB = async () => {
+  mongoose.connection.on("connected", () => console.log("Database Connected"));
 
-    mongoose.connection.on('connected', () => console.log('Database Connected'))
+  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
+};
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
-
-}
-
-export default connectDB
+export default connectDB;
